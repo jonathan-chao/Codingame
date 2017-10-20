a2917e6addda25fe34ff9118fefcade1710c6e07
Jonathan Chao <25756956+JChao7@users.noreply.github.com>
2017-10-19 17:05:28 -0700

    Update Coders Strike Back.py

--- Coders Strike Back.py
+++ Coders Strike Back.py
@@ -9,8 +9,8 @@ while True:
 
    #distance 
    # The checkpoints are circular, with a radius of 600 units.
-    if next_checkpoint_dist < 600: # how can distance be <15 if its radius is 600? if it will be in 15pix, it won't be nextcp anymore, u will have another cp's distance
-        thrust =10
+    if next_checkpoint_dist < 500: 
+        thrust =15
     else:
         thrust = 100
     
@@ -18,7 +18,7 @@ while True:
     if next_checkpoint_angle > 90 or next_checkpoint_angle < -90:
         thrust = 0
     #thrust when checkpoint distance is 3000 and angle is 0
-    if next_checkpoint_dist > 7000 and ( -5 < next_checkpoint_angle < 5):
+    if next_checkpoint_dist > 9000 and next_checkpoint_angle == 0: #( -5 < next_checkpoint_angle < 5):
         thrust = "BOOST"
             
     print(str(next_checkpoint_x) + " " + str(next_checkpoint_y) + " " + str(thrust))
